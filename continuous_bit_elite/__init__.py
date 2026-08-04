4ac2d749d44c7517040c97a8d7e7de570703e96a
ContinuousBitElite <continuous.bit.elite@gmail.com>
2026-08-04 20:05:45 +0200

    Bump version from 4.0.8 to 4.0.9

--- continuous_bit_elite/__init__.py
+++ continuous_bit_elite/__init__.py
@@ -7,7 +7,7 @@ from .core import UltraFastEngine
 from .blockchain import MerkleTree, ZeroKnowledgeProof, BlockchainVerifier
 from .compression import EliteCompressionAPI
 
-__version__ = "4.0.8"
+__version__ = "4.0.9"
 __all__ = [
     "UltraFastEngine",
     "MerkleTree",
