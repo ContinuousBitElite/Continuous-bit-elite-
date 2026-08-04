67639e3eb5608ed02b8bce78aa94c3008e57645f
ContinuousBitElite <continuous.bit.elite@gmail.com>
2026-08-04 01:21:34 +0100

    Initialize continuous_bit_elite module with imports

---
+++ continuous_bit_elite/__init__.py
@@ -0,0 +1,12 @@
+from .core import UltraFastEngine
+from .blockchain import MerkleTree, ZeroKnowledgeProof, BlockchainVerifier
+from .compression import EliteCompressionAPI
+
+__version__ = "4.0.0"
+__all__ = [
+    "UltraFastEngine",
+    "MerkleTree",
+    "ZeroKnowledgeProof",
+    "BlockchainVerifier",
+    "EliteCompressionAPI"
+]
