f1d760906db76bd43a9552d21f18818c07180786
ContinuousBitElite <continuous.bit.elite@gmail.com>
2026-08-10 15:50:15 +0200

    Update __init__.py

--- continuous_bit_elite/__init__.py
+++ continuous_bit_elite/__init__.py
@@ -7,7 +7,7 @@ from .core import UltraFastEngine
 from .blockchain import MerkleTree, ZeroKnowledgeProof, BlockchainVerifier
 from .compression import EliteCompressionAPI
 
-__version__ = "4.0.15"
+__version__ = "4.0.16"
 __all__ = [
     "UltraFastEngine",
     "MerkleTree",
